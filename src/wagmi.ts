8440959f1b075cb1cdcf0c9df13abd51d58069a4
sukanto01899 <sukanto01899@gmail.com>
2026-04-03 03:44:03 +0600

    feat: initial commit

---
+++ src/wagmi.ts
@@ -0,0 +1,25 @@
+import {
+  cookieStorage,
+  createConfig,
+  createStorage,
+  http,
+  injected,
+} from "wagmi";
+import { celo, celoSepolia } from "wagmi/chains";
+
+export function getConfig() {
+  return createConfig({
+    chains: [celo, celoSepolia],
+    connectors: [injected()],
+    transports: {
+      [celo.id]: http(),
+      [celoSepolia.id]: http(),
+    },
+  });
+}
+
+declare module "wagmi" {
+  interface Register {
+    config: ReturnType<typeof getConfig>;
+  }
+}
